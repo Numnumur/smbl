6a8c0cbc69c6c4e02aae291da3cecae9c9c29d16
Numnumur <nunuzero10@gmail.com>
2025-06-24 14:55:30 +0800

    modifying customer

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Customer extends Model
 {
@@ -12,4 +13,9 @@ class Customer extends Model
         'address',
         'note',
     ];
+
+    public function order(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
 }
